614a1107870c8a6022b7865ca2fea90d77227137
kumarkay.ca@gmail.com <Karan>
2020-01-10 16:10:34 -0500

    init

--- iOS_Maps_2/ViewController.swift
+++ iOS_Maps_2/ViewController.swift
@@ -7,14 +7,59 @@
 //
 
 import UIKit
+ import MapKit
+class ViewController: UIViewController,CLLocationManagerDelegate {
 
-class ViewController: UIViewController {
-
+    
+    var locationManager = CLLocationManager()
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let places = Place.getPlaces()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        addAnnotations()
+        mapView.showsUserLocation=true
+        
     }
 
+    
+    func addAnnotations(){
+        mapView.delegate = self
+        mapView.addAnnotations(places)
+        
+        let overlays = places.map { (MKCircle(center: $0.coordinate, radius: 100000))
+            
+        }
+        mapView.addOverlays(overlays)
+    }
 
+    // this func is used to add overlays
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer = MKCircleRenderer(overlay: overlay)
+        renderer.fillColor = UIColor.black.withAlphaComponent(0.5)
+        renderer.strokeColor = UIColor.green
+        renderer.lineWidth = 2
+        return renderer
+    }
+ 
 }
 
+extension ViewController:MKMapViewDelegate{
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation{
+            return nil
+        }
+        else{
+            let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "annotationView") ?? MKAnnotationView()
+            annotationView.image=UIImage(named: "ic_place")
+        return annotationView
+        }
+    }
+ }
+ 
