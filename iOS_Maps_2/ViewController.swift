4cb728cb08a1d27a9e6afd6ab2d4c9a0c0449770
kumarkay.ca@gmail.com <Karan>
2020-01-10 16:30:19 -0500

    lines between points

--- iOS_Maps_2/ViewController.swift
+++ iOS_Maps_2/ViewController.swift
@@ -25,15 +25,22 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
         locationManager.startUpdatingLocation()
         addAnnotations()
         mapView.showsUserLocation=true
+        addPolyLine()
         
     }
 
+    
+    func addPolyLine() {
+        let locations = places.map{$0.coordinate}
+        let polyline = MKPolyline(coordinates: locations, count: locations.count)
+        mapView.addOverlay(polyline)
+    }
     
     func addAnnotations(){
         mapView.delegate = self
         mapView.addAnnotations(places)
         
-        let overlays = places.map { (MKCircle(center: $0.coordinate, radius: 100000))
+        let overlays = places.map { (MKCircle(center: $0.coordinate, radius: 10000))
             
         }
         mapView.addOverlays(overlays)
@@ -41,11 +48,21 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
 
     // this func is used to add overlays
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if overlay is MKCircle{
         let renderer = MKCircleRenderer(overlay: overlay)
         renderer.fillColor = UIColor.black.withAlphaComponent(0.5)
         renderer.strokeColor = UIColor.green
         renderer.lineWidth = 2
         return renderer
+        }
+        else if overlay is MKPolyline{
+            let renderer = MKPolylineRenderer(overlay: overlay)
+            renderer.strokeColor = UIColor.red
+            renderer.lineWidth = 3
+            return renderer
+            
+        }
+        return MKOverlayRenderer()
     }
  
 }
